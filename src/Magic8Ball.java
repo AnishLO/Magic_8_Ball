169cbd15064fb86ec14f1a71092349919b7f0f6c
Anish <Anish.lohar.2022@student.lhschools.org>
2019-10-03 09:05:51 -0400

    Added verbals

---
+++ src/Magic8Ball.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[]args){
+        //todo Declare verbals
+        Scanner keyboard;
+        int value;
+        int testing;
+        String question;
+
+        //todo intialization  phase
+
+
+
+
+
+
+
+
+    }
+}
