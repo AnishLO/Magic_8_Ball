0ed5add45b1a674866b85ad477cdb4e0fd268b33
Anish <Anish.lohar.2022@student.lhschools.org>
2019-10-03 10:11:10 -0400

    added verbals

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -5,11 +5,32 @@ public class Magic8Ball {
     public static void main(String[]args){
         //todo Declare verbals
         Scanner keyboard;
-        int value;
-        int testing;
-        String question;
+         double number;
+        keyboard = new Scanner(System.in);
 
-        //todo intialization  phase
+
+        System.out.println("This is the magic 8ball");
+        System.out.println("Give an number between 1-100");
+        System.out.println("when you give the nubmer you'll get the answer to every one of your question");
+        number = keyboard.nextDouble();
+
+
+        if (10>=number && number>0); {
+            System.out.println("Most likely not");
+
+        }
+         else if (20<number && number>10); {
+              System.out.println("Most likely yes");
+
+        }
+        else if (30<number && number>20); {
+            System.out.println("certian");
+        }
+
+
+
+
+
 
 
 
